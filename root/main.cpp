a46387b4a463d02009ad26dab019043291706931
Francisco Parente <root.emanuel@hotmail.com>
2017-12-23 19:25:28 -0200

    Initial Commit

---
+++ root/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  root
+//
+//  Created by Emanuel Root on 23/12/17.
+//  Copyright © 2017 Emanuel Root. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
